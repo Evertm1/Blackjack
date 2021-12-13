1ede33c00178027d1ec0b968ae026bea6fd27f73
Programming <programming@Marcos-MacBook-Pro.local>
2021-12-13 10:14:03 -0800

    cached elements, added event listeners to buttons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-console.log('js is connected')
+// script testing
+console.log('js is connected');
+
+let clickTest = () => {
+    console.log('a button has been pressed')
+};
 
 
 // define constants
-
+//deck
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
@@ -15,18 +20,34 @@ let winner = null;
 let playerTotal = 0;
 let dealerTotal = 0;
 
-// cache elements
+// cached elements
+playerHandEl = document.querySelector('#playerhand');
+playerTotalEl = document.querySelector('#playertotal');
+dealerHandEl = document.querySelector('#dealerhand');
+dealerTotalEl = document.querySelector('#dealertotal');
+winLoseEl = document.querySelector('#winlose');
+dealerStaysEl = document.querySelector('#dealerstays');
 
-//buttons
-const hitMeEl = document.querySelector('hitme');
-const stayEl = document.querySelector('stay');
-const playAgainEl = document.querySelector('playagain');
-//hand containers
-
-//totals containers
-
+// cached button elements
+const hitMeBtn = document.querySelector('#hitme').addEventListener("click", clickTest);
+const stayBtn = document.querySelector('#stay').addEventListener("click", clickTest);
+const playAgainBtn = document.querySelector('#playagain').addEventListener("click", clickTest);;
 
 //functions
+// initialize page upon loading
+init();
+
+function init() {
+    //initialize master deck
+    //shuffle deck
+    //add two cards to playerHand
+    //add two cards to dealerHand
+    //calculate playerTotal
+    //calculate dealerTotal
+    //deactivate playAgainBtn
+}
+
+
 
 
 
