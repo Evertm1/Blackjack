a0b34784f4019c038284c748b803e80b4b580350
Programming <programming@Marcos-MacBook-Pro.local>
2021-12-13 09:06:32 -0800

    started caching variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 console.log('js is connected')
+
+
+// define constants
+
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+
+// define variables
+
+let playerHand = [];
+let dealerHand = [];
+let winner = null;
+let playerTotal = 0;
+let dealerTotal = 0;
+
+// cache elements
+
+//buttons
+const hitMeEl = document.querySelector('hitme');
+const stayEl = document.querySelector('stay');
+const playAgainEl = document.querySelector('playagain');
+//hand containers
+
+//totals containers
+
+
+//functions
+
+
+
